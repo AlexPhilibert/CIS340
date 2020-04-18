08978571c7e27c23a297cfcb641b5767bfce4c0f
AlexPhilibert <63882339+AlexPhilibert@users.noreply.github.com>
2020-04-18 00:15:46 +0000

    My Second Project Week 13

---
+++ MySecondProject/App copy 2.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, TextInput, View } from 'react-native';
+
+export default function MyApp() {
+
+  return (
+    <View>
+      <Text>
+        {"\n\n\n\n\n\n"}
+        Hello, I am a student in CIS340! {"\n"}
+      </Text>
+    <TextInput 
+    styly = {{
+      gwight: 40,
+      borderColor: "red",
+      borderWidth: 1
+    }}
+
+    />
+    </View>
+  );
+}
+
+
+ 
---
+++ MySecondProject/App copy.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function MyApp() {
+
+  function getFullName(fName,mName,lName){
+     return fName + " " + mName + " "+ lName;
+  }
+
+  const pet = "Dog";
+  return (
+    
+      <Text>
+        {"\n\n\n\n\n\n"}
+        Hello, I am a student in CIS340! {"\n"}
+        My full name is {getFullName("Alex","John","Philibert")}{"\n"}
+        I have a {pet}!
+      </Text>
+    
+  );
+}
+
+
+ 
